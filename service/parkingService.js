115ddff9cc1a6dd754a43fc8b925a7e6291486ba
Bavornwut Putchong <earth.igniter@gmail.com>
2022-09-30 22:22:51 +0700

    refactor code

--- service/parkingService.js
+++ service/parkingService.js
@@ -1,5 +1,6 @@
 const ParkingLot = require("../models/parking_lot.model");
 const getParkingStatus = () => ParkingLot.find();
+
 const findNearestParkingSlot = async (floor, carSize) => {
   const parkingLots = await ParkingLot.aggregate([
     {
@@ -55,41 +56,26 @@ const allocatedParkingSlot = async (parkingLotId, size, slotId) => {
 
 const createParkingLot = (params) => {
   const { name, floor, slots } = params;
-  const { s, m, l, xl } = slots;
-  const slot = {
-    s: [],
-    m: [],
-    l: [],
-    xl: [],
-  };
-  for (let i = 1; i <= s; i++) {
-    slot.s.push({
-      slotID: i,
-      status: "available",
-    });
-  }
-  for (let i = 1; i <= m; i++) {
-    slot.m.push({
-      slotID: i + slot.s.length,
-      status: "available",
-    });
-  }
-  for (let i = 1; i <= l; i++) {
-    slot.l.push({
-      slotID: i + slot.m.length + slot.s.length,
-      status: "available",
-    });
-  }
-  for (let i = 1; i <= xl; i++) {
-    slot.xl.push({
-      slotID: i + slot.l.length + slot.m.length + slot.s.length,
-      status: "available",
-    });
-  }
+  const parkingSlot = new Map();
+  let slotSize = 0;
+  Object.keys(slots).forEach((value, key) => {
+    let slot = [];
+    for (let i = 1; i <= Object.values(slots)[key]; i++) {
+      slotSize++;
+      slot.push({
+        slotID: slotSize,
+        status: "available",
+      });
+    }
+    parkingSlot.set(value, slot);
+  });
+
+  const parkingSlotObj = Object.fromEntries(parkingSlot);
+
   return new ParkingLot({
     name,
     floor,
-    slots: slot,
+    slots: parkingSlotObj,
   }).save();
 };
 
